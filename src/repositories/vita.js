281663454cc961eed4b1be7c36ccc2df6626f28e
JacquesRaoult <raoult.jacques@gmail.com>
2026-02-08 16:02:34 +0100

    Expose img in vita API

--- src/repositories/vita.js
+++ src/repositories/vita.js
@@ -3,7 +3,7 @@ import { db } from "../utils/database.js"
 export const synaxarVita = {
     async getVita(id) {
         const rows = await db`
-                SELECT vie_b, vita_long, vita_liturgy
+                SELECT vie_b, vita_long, vita_liturgy, img
                 FROM vies
                 WHERE id = ${id}
             `
