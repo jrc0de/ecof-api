807020998925312eb701127a060c328fa6f720b3
JacquesRaoult <raoult.jacques@gmail.com>
2026-04-09 19:54:41 +0200

    Edit monitoring route

--- src/routes/vitaMonitoring.ts
+++ src/routes/vitaMonitoring.ts
@@ -13,7 +13,7 @@ synaxarRoutes.get("/", async (c) => {
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Vies liturgiques</title>
+    <title>Monitoring</title>
     <style>
         body {
             font-family: 'Courier New', monospace;
