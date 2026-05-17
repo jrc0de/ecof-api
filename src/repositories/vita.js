f461291520b177ade687d3e9280d6ceec5dc183e
JacquesRaoult <raoult.jacques@gmail.com>
2026-05-17 08:14:54 +0200

    Refactoring vita api

--- src/repositories/vita.js
+++ src/repositories/vita.js
@@ -1,23 +1,14 @@
 import { db } from "../utils/database"
-import { parseMarkdown } from "../utils/markdown"
 
 export const synaxarVita = {
     async getVita(id) {
         const rows = await db`
-      SELECT v_short as vie_b, v_long as vita_long, v_liturgy as vita_liturgy,
-             img, mois, jour,
-             prefixe, saint
-      FROM synaxaire
-      WHERE id = ${id}
-    `
-        const row = rows[0]
-        if (!row) return null
-
-        return {
-            ...row,
-            vie_b: parseMarkdown(row.vie_b),
-            vita_long: parseMarkdown(row.vita_long),
-            vita_liturgy: parseMarkdown(row.vita_liturgy),
-        }
+          SELECT v_short as vie_b, v_long as vita_long, v_liturgy as vita_liturgy,
+                 img, mois, jour,
+                 prefixe, saint
+          FROM synaxaire
+          WHERE id = ${id}
+        `
+        return rows[0] ?? null
     },
 }
--- src/services/vita.js
+++ src/services/vita.js
@@ -1,6 +1,14 @@
 import { synaxarVita } from "../repositories/vita.js"
+import { parseMarkdown } from "../utils/markdown.js"
 
 export async function getSaintLives(id) {
     const vita = await synaxarVita.getVita(id)
-    return vita
+    if (!vita) return null
+
+    return {
+        ...vita,
+        vie_b: parseMarkdown(vita.vie_b),
+        vita_long: parseMarkdown(vita.vita_long),
+        vita_liturgy: parseMarkdown(vita.vita_liturgy),
+    }
 }
